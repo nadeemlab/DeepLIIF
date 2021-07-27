c986b79c4d40b3fe6b0814510d144722900decd0
Parmida93 <ghahremani.parmida@gmail.com>
2021-07-27 19:28:17 -0400

    Update download_training_sets.sh

--- Scripts/download_training_sets.sh
+++ Scripts/download_training_sets.sh
@@ -4,14 +4,15 @@ cd ./Datasets/Dataset1/train/
 curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/DeepLIIF_Training_Set.zip?download=1" --output DeepLIIF_Training_Set.zip
 curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/BC-DeepLIIF_Training_Set.zip?download=1" --output BC-DeepLIIF_Training_Set.zip
 
+sudo apt-get install unzip
+unzip DeepLIIF_Training_Set.zip
+unzip BC-DeepLIIF_Training_Set.zip
+
 cd ../../..
 
-sudo apt-get install unzip
-unzip ./Datasets/Dataset1/train/DeepLIIF_Training_Set.zip
 mv ./Datasets/Dataset1/train/DeepLIIF_Training_Set/*.png ./Datasets/Dataset1/train/
 rm ./Datasets/Dataset1/train/DeepLIIF_Training_Set.zip
 
-unzip ./Datasets/Dataset1/train/BC-DeepLIIF_Training_Set.zip
 mv ./Datasets/Dataset1/train/BC-DeepLIIF_Training_Set/*.png ./Datasets/Dataset1/train/
 rm ./Datasets/Dataset1/train/BC-DeepLIIF_Training_Set.zip
 
@@ -23,13 +24,14 @@ cd ./Datasets/Dataset1/val/
 curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/DeepLIIF_Validation_Set.zip?download=1" --output DeepLIIF_Validation_Set.zip
 curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/BC-DeepLIIF_Validation_Set.zip?download=1" --output BC-DeepLIIF_Validation_Set.zip
 
+sudo apt-get install unzip
+unzip DeepLIIF_Validation_Set.zip
+unzip BC-DeepLIIF_Validation_Set.zip
+
 cd ../../..
 
-sudo apt-get install unzip
-unzip ./Datasets/Dataset1/val/DeepLIIF_Validation_Set.zip
 mv ./Datasets/Dataset1/val/DeepLIIF_Validation_Set/*.png ./Datasets/Dataset1/train/
 rm ./Datasets/Dataset1/val/DeepLIIF_Validation_Set.zip
 
-unzip ./Datasets/Dataset1/val/BC-DeepLIIF_Validation_Set.zip
 mv ./Datasets/Dataset1/val/BC-DeepLIIF_Validation_Set/*.png ./Datasets/Dataset1/train/
 rm ./Datasets/Dataset1/val/BC-DeepLIIF_Validation_Set.zip
