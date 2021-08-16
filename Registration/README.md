661dabf97e6fee8d7f0c2da0baaad219a84df4ef
Parmida93 <ghahremani.parmida@gmail.com>
2021-08-16 15:43:33 -0400

    Registration App Updated! Registration README added!

---
+++ Registration/README.md
@@ -0,0 +1,51 @@
+### DeepLIIF: Deep-Learning Inferred Multiplex Immunofluoresence for IHC Quantification
+
+[Read Link](https://www.biorxiv.org/content/10.1101/2021.05.01.442219v1) | [Google CoLab Demo](https://colab.research.google.com/drive/12zFfL7rDAtXfzBwArh9hb0jvA38L_ODK?usp=sharing)
+
+*Reporting biomarkers assessed by routine immunohistochemical (IHC) staining of tissue is broadly used in diagnostic pathology laboratories for patient care. To date, clinical reporting is predominantly qualitative or semi-quantitative. By creating a multitask deep learning framework referred to as DeepLIIF, we are presenting a single step solution to nuclear segmentation and quantitative single-cell IHC scoring. Leveraging a unique de novo dataset of co-registered IHC and multiplex immunoflourescence (mpIF) data generated from the same tissue section, we simultaneously segment and translate low-cost and prevalent IHC slides to more expensive-yet-informative mpIF images. Moreover, a nuclear-pore marker, LAP2beta, is co-registered to improve cell segmentation and protein expression quantification on IHC slides. By formulating the IHC quantification as cell instance segmentation/classification rather than cell detection problem, we show that our model trained on clean IHC Ki67 data can generalize to more noisy and artifact-ridden images as well as other nuclear and non-nuclear markers such as CD3, CD8, BCL2, BCL6, MYC, MUM1, CD10 and TP53. We thoroughly evaluate our method on publicly available benchmark datasets as well as against pathologists’ semi-quantitative scoring.*
+
+© This code is made available for non-commercial academic purposes.
+
+![overview_image](./images/overview.png)*Figure 1. Overview of DeepLIIF pipeline and sample input IHCs (different brown/DAB markers -- BCL2, BCL6, CD10, CD3/CD8, Ki67) with corresponding DeepLIIF-generated hematoxylin/mpIF modalities and classified (positive (red) and negative (blue) cell) segmentation masks. (a) Overview of DeepLIIF. Given an IHC input, our multitask deep learning framework simultaneously infers corresponding Hematoxylin channel, mpIF DAPI, mpIF protein expression (Ki67, CD3, CD8, etc.), and the positive/negative protein cell segmentation, baking explainability and interpretability into the model itself rather than relying on coarse activation/attention maps. In the segmentation mask, the red cells denote cells with positive protein expression (brown/DAB cells in the input IHC), whereas blue cells represent negative cells (blue cells in the input IHC). (b) Example DeepLIIF-generated hematoxylin/mpIF modalities and segmentation masks for different IHC markers. DeepLIIF, trained on clean IHC Ki67 nuclear marker images, can generalize to noisier as well as other IHC nuclear/cytoplasmic marker images.*
+
+## Prerequisites:
+```del
+Python 3
+tk
+Pillow>=7.1.2
+matplotlib>=3.3.3
+```
+## Registration Framework
+* To open the registration framework please run the following command:
+```
+python Registration_App.py
+```
+
+* To register the images, you have to first load the base image using the 'Open Base Image' button, and load the image you want to register over the base image using 'Open Moving Image' button.
+* On the left side, you can see three viewers for visualizing: 1) the base image, 2) the moving image, 3) the moving image overlaid on the base image.
+* Ater loading both images, you will see the moving image overlaid on top of the base image.
+* **Translation:** Using the 'Left', 'Right', 'Up', and 'Down' buttons, you can move the moving image to the left, right, up, and down. You can change the translation value using the text box in the middle of the alignment buttons.
+* **Scaling:** Using the 'Zoom in' and 'Zoom out' button, you can change the scale of the moving image. You can set the vertical and horizontal scaling value using the provided text boxes under the zoom buttons.
+* **Rotation:** You can rotate the moving image using the provided buttons.
+
+
+## Issues
+Please report all issues on the public forum.
+
+## License
+© DeepLIIF code is distributed under **Apache 2.0 with Commons Clause** license, and is available for non-commercial academic purposes. 
+
+## Acknowledgments
+* This code is inspired by [CycleGAN and pix2pix in PyTorch](https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix).
+
+## Reference
+If you find our work useful in your research or if you use parts of this code please consider citing our paper:
+```
+@article{ghahremani2021deepliif,
+  title={DeepLIIF: Deep Learning-Inferred Multiplex ImmunoFluorescence for IHC Quantification},
+  author={Ghahremani, Parmida and Li, Yanyun and Kaufman, Arie and Vanguri, Rami and Greenwald, Noah and Angelo, Michael and Hollmann, Travis J and Nadeem, Saad},
+  journal={bioRxiv},
+  year={2021},
+  publisher={Cold Spring Harbor Laboratory}
+}
+```
--- Registration/Registration_App.py
+++ Registration/Registration_App.py
@@ -24,21 +24,11 @@ from matplotlib.image import imsave
 class RegistrationApp:
     def __init__(self, window, window_title):
 
-        self.hematoxylin = [0.651, 0.701, 0.29]
-        self.eosin = [0.216, 0.801, 0.558]
-        self.color_deconv = np.array([[0.65, 0.70, 0.29], [0.216, 0.801, 0.558], [0.316, 0.598, 0.737]])
-
-        self.cmap_hema = LinearSegmentedColormap.from_list('mycmap', ['green', 'white'])
-        self.cmap_eosin = LinearSegmentedColormap.from_list('mycmap', ['maroon', 'white'])
-        self.cmap_dab = LinearSegmentedColormap.from_list('mycmap', ['white', 'saddlebrown'])
-
-        self.maxIntensity = 255
-        self.phi = 1
-        self.theta = 1
-
         self.alignment_values = [0, 0]
         self.start_index = [0, 0]
+        self.start_index = [0, 0]
         self.zoom_value = 0
+        self.rotate_value = 0
 
         self.padding = 10
 
@@ -57,26 +47,21 @@ class RegistrationApp:
 
         # Create a canvas that can fit the above image
         self.canvas_base = tk.Canvas(self.window, width=self.base_canvas_size, height=self.base_canvas_size)
-        # self.canvas_base.grid(row=0, column=0)
         self.canvas_base.place(x=self.padding, y=self.padding)
 
         self.canvas_moving = tk.Canvas(self.window, width=self.moving_canvas_size, height=self.moving_canvas_size)
-        # self.canvas_moving.grid(row=0, column=1)
         self.canvas_moving.place(x=self.base_canvas_size+self.padding, y=self.padding)
 
         self.canvas_blended = tk.Canvas(self.window, width=self.blended_canvas_size, height=self.blended_canvas_size)
-        # self.canvas_blended.grid(row=1, column=0)
         self.canvas_blended.place(x=self.padding, y=self.moving_canvas_size+self.padding)
 
         self.initial_load()
 
         self.load_base_image_button = tk.Button(self.window, text="Open Base Image", command=lambda event='base': self.open_images(event), width=15, height=2)
-        # self.next_button.grid(row=1, column=1)
         self.window.bind('l', self.open_images)
         self.load_base_image_button.place(y=int(screen_height / 8), x=int(screen_height * 3 / 4) - 115)
 
         self.load_moving_image_button = tk.Button(self.window, text="Open Moving Image", command=lambda event='moving': self.open_images(event), width=15, height=2)
-        # self.next_button.grid(row=1, column=1)
         self.window.bind('l', self.open_images)
         self.load_moving_image_button.place(y=int(screen_height / 8), x=int(screen_height * 3 / 4) + 55)
 
@@ -91,7 +76,6 @@ class RegistrationApp:
         # self.load_moving_wsi_button.place(y=int(screen_height / 8) + 40, x=int(screen_height * 3 / 4) + 55)
 
         self.save_button = tk.Button(self.window, text="Save", command=self.save_image, width=10, height=2)
-        # self.save_button.grid(row=1, column=2)
         self.window.bind('s', self.save_image)
         self.save_button.place(y=int(screen_height / 8) + 100, x=int(screen_height * 3 / 4) - 10)
 
@@ -106,62 +90,60 @@ class RegistrationApp:
         self.moving_val_text_box.insert(0, '1')
 
         self.down_button = tk.Button(self.window, text="Down", command=self.down, width=10, height=2)
-        # self.down_button.grid(row=2, column=1)
         self.window.bind('<Down>', self.down)
         self.down_button.place(y=int(screen_height / 8) + 280, x=int(screen_height * 3 / 4) - 10)
-        # self.down_button.pack()
 
         self.up_button = tk.Button(self.window, text="Up", command=self.up, width=10, height=2)
-        # self.up_button.grid(row=2, column=2)
         self.window.bind('<Up>', self.up)
         self.up_button.place(y=int(screen_height / 8) + 200, x=int(screen_height * 3 / 4) - 10)
-        # self.up_button.pack(anchor=tkinter.CENTER)
 
         self.left_button = tk.Button(self.window, text="Left", command=self.left, width=10, height=2)
-        # self.left_button.grid(row=3, column=1)
         self.window.bind('<Left>', self.left)
         self.left_button.place(y=int(screen_height / 8) + 240, x=int(screen_height * 3 / 4) - 85)
 
         self.right_button = tk.Button(self.window, text="Right", command=self.right, width=10, height=2)
-        # self.right_button.grid(row=3, column=2)
         self.window.bind('<Right>', self.right)
         self.right_button.place(y=int(screen_height / 8) + 240, x=int(screen_height * 3 / 4) + 65)
 
         self.zoom_in_button = tk.Button(self.window, text="Zoom in", command=self.zoom_in, width=10, height=2)
-        # self.right_button.grid(row=3, column=2)
         self.window.bind('z', self.zoom_in)
         self.zoom_in_button.place(y=int(screen_height / 8) + 340, x=int(screen_height * 3 / 4) - 65)
 
         self.zoom_out_button = tk.Button(self.window, text="Zoom out", command=self.zoom_out, width=10, height=2)
-        # self.right_button.grid(row=3, column=2)
         self.window.bind('z', self.zoom_in)
         self.zoom_out_button.place(y=int(screen_height / 8) + 340, x=int(screen_height * 3 / 4) + 55)
 
         zoom_x_var = tk.StringVar()
         self.label_zoom_x = tk.Label(window, textvariable=zoom_x_var)
-        zoom_x_var.set('Zoom X Value:')
-        self.label_zoom_x.place(y=int(screen_height / 8) + 400, x=int(screen_height * 3 / 4) - 65)
+        zoom_x_var.set('Horizontal Value:')
+        self.label_zoom_x.place(y=int(screen_height / 8) + 400, x=int(screen_height * 3 / 4) - 75)
 
         self.zoom_x_text_box = tk.Entry(window, width=2)
-        self.zoom_x_text_box.place(y=int(screen_height / 8) + 400, x=int(screen_height * 3 / 4) + 55)
+        self.zoom_x_text_box.place(y=int(screen_height / 8) + 400, x=int(screen_height * 3 / 4) + 85)
         self.zoom_x_text_box.insert(0, '3')
 
         zoom_y_var = tk.StringVar()
         self.label_zoom_y = tk.Label(window, textvariable=zoom_y_var)
-        zoom_y_var.set('Zoom Y Value:')
-        self.label_zoom_y.place(y=int(screen_height / 8) + 420, x=int(screen_height * 3 / 4) - 65)
+        zoom_y_var.set('Vertical Value:')
+        self.label_zoom_y.place(y=int(screen_height / 8) + 420, x=int(screen_height * 3 / 4) - 75)
 
         self.zoom_y_text_box = tk.Entry(window, width=2)
-        self.zoom_y_text_box.place(y=int(screen_height / 8) + 420, x=int(screen_height * 3 / 4) + 55)
+        self.zoom_y_text_box.place(y=int(screen_height / 8) + 420, x=int(screen_height * 3 / 4) + 85)
         self.zoom_y_text_box.insert(0, '4')
 
-        self.moving_val = 1
-        self.moving_val_bulk = 10
-        # self.window.bind('z', self.zoom)
-        # self.window.bind('x', self.zoom_x)
-        # self.window.bind('y', self.zoom_y)
-        self.window.mainloop()
+        self.rotate_right_button = tk.Button(self.window, text="Rotate Right", command=self.rotate_right, width=10, height=2)
+        self.window.bind('z', self.zoom_in)
+        self.rotate_right_button.place(y=int(screen_height / 8) + 480, x=int(screen_height * 3 / 4) + 70)
 
+        self.rotate_value_text_box = tk.Entry(window, width=3)
+        self.rotate_value_text_box.place(y=int(screen_height / 8) + 490, x=int(screen_height * 3 / 4) + 35)
+        self.rotate_value_text_box.insert(0, '90')
+
+        self.rotate_left_button = tk.Button(self.window, text="Rotate Left", command=self.rotate_left, width=10, height=2)
+        self.window.bind('z', self.zoom_in)
+        self.rotate_left_button.place(y=int(screen_height / 8) + 480, x=int(screen_height * 3 / 4) - 85)
+
+        self.window.mainloop()
 
     def open_images(self, event):
         self.image_index = 0
@@ -200,8 +182,11 @@ class RegistrationApp:
                                          image=self.tk_blended_img)
 
     def reload_moving_image(self):
+        rotated_image = self.moving_img.copy()
+        rotated_image = rotated_image.rotate(self.rotate_value)
+
         crop_aligned = Image.new('RGB', self.moving_img.size)
-        aligned_image = self.moving_img.copy()
+        aligned_image = rotated_image.copy()
         aligned_image = aligned_image.crop((max(self.start_index[1] - self.alignment_values[1], 0),
                                             max(self.start_index[0] - self.alignment_values[0], 0),
                                             min(self.moving_img.size[1] - self.alignment_values[1], self.moving_img.size[1]),
@@ -236,57 +221,6 @@ class RegistrationApp:
         print(self.zoom_value)
         # self.crop_moving.show()
         self.create_blended_image()
-
-    def blend_image_files(self, filename1, filename2):
-        background = Image.open(filename2)
-        overlay = Image.open(filename1)
-
-        background = background.convert("RGBA")
-        overlay = overlay.convert("RGBA")
-
-        new_img = Image.blend(background, overlay, 0.4)
-        return new_img
-
-    def blend_images(self, background, overlay):
-        # background = background.convert("RGBA")
-        # overlay = overlay.convert("RGBA")
-        # overlay = cv2.cvtColor(1-cv2.cvtColor(overlay, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
-        #
-        # new_img = cv2.addWeighted(background, 0.3, overlay, 0.7, 0)
-        size_x = min(background.shape[0], overlay.shape[0])
-        size_y = min(background.shape[1], overlay.shape[1])
-        background = background[:size_x, :size_y]
-        overlay = overlay[:size_x, :size_y]
-        overlay = cv2.cvtColor(overlay, cv2.COLOR_GRAY2BGR)
-        background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
-        # background[:,:,2] = 0
-        background[:,:,1] = 0
-        # new_img = np.zeros((size_x, size_y, 3))
-        # new_img[:,:,2] = overlay
-        # new_img[:,:,1] = background
-        new_img = cv2.addWeighted(background, 0.7, overlay, 0.3, 0)
-        return new_img
-
-    def save_channel(self, img, filename, channel):
-        # Create an artificial color close to the orginal one
-        # cmap_hema = LinearSegmentedColormap.from_list('mycmap', ['darkmagenta', 'white'])
-        # cmap_eosin = LinearSegmentedColormap.from_list('mycmap', ['teal', 'white'])
-        # cmap_dab = LinearSegmentedColormap.from_list('mycmap', ['white', 'saddlebrown'])
-
-        if channel == 'hema':
-            selected_cmap = self.cmap_hema
-        elif channel == 'eosin':
-            selected_cmap = self.cmap_eosin
-        else:
-            selected_cmap = self.cmap_dab
-
-        my_dpi = 200
-        fig = plt.figure(figsize=(self.width/my_dpi, self.height/my_dpi), dpi=my_dpi)
-        ax = plt.Axes(fig, [0., 0., 1., 1.])
-        ax.set_axis_off()
-        fig.add_axes(ax)
-        ax.imshow(img, cmap=selected_cmap)
-        fig.savefig(filename, dpi=my_dpi)
 
     def save_image(self):
         print('SAVE CLICKED')
@@ -352,24 +286,20 @@ class RegistrationApp:
         self.alignment_values[1] += self.moving_val
         self.reload_moving_image()
 
-    def zoom_y(self):
-        print('in zoom')
-        self.image_size = self.blue_img.shape
-        self.blue_img = cv2.resize(self.blue_img[:self.image_size[0], 1:self.image_size[1] - 1], (self.image_size[1], self.image_size[0]), interpolation=cv2.INTER_AREA)
-        self.reload_moving_image()
-
-    def zoom_x(self):
-        print('in zoom')
-        self.image_size = self.blue_img.shape
-        self.blue_img = cv2.resize(self.blue_img[1:self.image_size[0]-1, :self.image_size[1]], (self.image_size[1], self.image_size[0]), interpolation=cv2.INTER_AREA)
-        self.reload_moving_image()
-
     def zoom_in(self):
         self.zoom_value += 1
         self.reload_moving_image()
 
     def zoom_out(self):
         self.zoom_value -= 1
+        self.reload_moving_image()
+
+    def rotate_right(self):
+        self.rotate_value -= int(self.rotate_value_text_box.get())
+        self.reload_moving_image()
+
+    def rotate_left(self):
+        self.rotate_value += int(self.rotate_value_text_box.get())
         self.reload_moving_image()
 
     def adjust_gamma(self, image, gamma=1.0):
