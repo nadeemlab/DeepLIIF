de2c9b3d612edadc6d286eea59877a1a828d126d
Parmida93 <ghahremani.parmida@gmail.com>
2021-09-15 15:33:28 -0400

    Download_Pre_Trained_Model Script Updated!

--- Scripts/download_pre_trained_model.sh
+++ Scripts/download_pre_trained_model.sh
@@ -1,12 +1,12 @@
 mkdir -p ./checkpoints/
 cd ./checkpoints/
-curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/DeepLIIF_Model.zip?download=1" --output DeepLIIF_Model.zip
+curl --cookie zenodo-cookies.txt "https://zenodo.org/record/4751737/files/DeepLIIF_Latest_Model.zip?download=1" --output DeepLIIF_Latest_Model.zip
 
 sudo apt-get install unzip
-unzip DeepLIIF_Model.zip
+unzip DeepLIIF_Latest_Model.zip
 
 cd ..
 
 echo "Downloaded pre-trained files:"
-ls ./checkpoints/DeepLIIF_Model/
-rm ./checkpoints/DeepLIIF_Model.zip
+ls ./checkpoints/DeepLIIF_Latest_Model/
+rm ./checkpoints/DeepLIIF_Latest_Model.zip
