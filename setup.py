a8df2e9ab841cee9ea187ea8b5e33a1e3fff7bfc
Joseph Marino <88632875+jommarin@users.noreply.github.com>
2021-10-09 02:00:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
         'opencv-python==4.5.3.56',
         'torchvision==0.10.0',
         'scikit-image==0.18.3',
-        'dominate==2.6.0'
+        'dominate==2.6.0',
+        'numba==0.53.1'
     ]
 )
