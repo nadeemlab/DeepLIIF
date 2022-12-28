5355fb4cc4abdf1de82dacf02b49aeea924e756f
Wendy Wang <wanting.wang@columbia.edu>
2022-12-28 03:07:23 +0000

    fix: use batch stats not running stats in batchnorm2d layers for inference; set model on eval mode and add test for torchscript serialization; add eager mode flag for cli.py test command

--- test.py
+++ test.py
@@ -55,8 +55,9 @@ if __name__ == '__main__':
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
     # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
-    if opt.eval:
-        model.eval()
+    model.eval()
+    # if opt.eval:
+    #     #model.eval()
 
     _start_time = time.time()
 
