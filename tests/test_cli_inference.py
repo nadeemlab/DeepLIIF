ea64ee0896618a8065eb6f5ffcc9ac0ef83276e6
Wendy Wang <wanting.wang@columbia.edu>
2023-07-04 08:42:26 -0400

    added tolerance

--- tests/test_cli_inference.py
+++ tests/test_cli_inference.py
@@ -6,6 +6,44 @@ import cv2
 import os
 import numpy as np
 
+
+TOLERANCE = 0.0001
+
+def calculate_ssim(dir_a,dir_b,fns,suffix_a,suffix_b,verbose_freq=50):
+    print(suffix_a, suffix_b)
+    score = 0
+    count = 0
+    
+    for fn in fns:
+        path_a = f"{dir_a}/{fn}{suffix_a}.png"
+        assert os.path.exists(path_a), f'path {path_a} does not exist'
+        img_a = cv2.imread(path_a)
+        # print(img_a.shape)
+        
+        if suffix_b == 'combine': 
+            paths_b = [f"{dir_b}/{fn}fake_BS_{i}.png" for i in range(1,5)]
+            imgs_b = []
+            for path_b in paths_b:
+                assert os.path.exists(path_b), f'path {path_b} does not exist'
+                imgs_b.append(cv2.imread(path_b))
+            img_b = np.mean(imgs_b,axis=(0))
+        else:
+            path_b = f"{dir_b}/{fn}{suffix_b}.png"
+            assert os.path.exists(path_b), f'path {path_b} does not exist'
+            img_b = cv2.imread(path_b)
+        # print(img_b.shape)
+        
+        score += ssim(img_a,img_b, data_range=img_a.max() - img_b.min(), multichannel=True, channel_axis=2)
+        count +=1
+
+        if count % verbose_freq == 0 or count == len(fns):
+            print(f"{count}/{len(fns)}, running mean SSIM {score / count}")
+    return score/count
+
+
+
+
+
 def test_cli_inference(tmp_path, model_dir_final):
     dir_model = model_dir_final
     dir_input = 'Datasets/Sample_Dataset/test_cli'
@@ -40,37 +78,6 @@ def test_cli_inference_eager(tmp_path, model_dir_final):
     assert num_output == num_input * 7
 
 
-def calculate_ssim(dir_a,dir_b,fns,suffix_a,suffix_b,verbose_freq=50):
-    print(suffix_a, suffix_b)
-    score = 0
-    count = 0
-    
-    for fn in fns:
-        path_a = f"{dir_a}/{fn}{suffix_a}.png"
-        assert os.path.exists(path_a), f'path {path_a} does not exist'
-        img_a = cv2.imread(path_a)
-        # print(img_a.shape)
-        
-        if suffix_b == 'combine': 
-            paths_b = [f"{dir_b}/{fn}fake_BS_{i}.png" for i in range(1,5)]
-            imgs_b = []
-            for path_b in paths_b:
-                assert os.path.exists(path_b), f'path {path_b} does not exist'
-                imgs_b.append(cv2.imread(path_b))
-            img_b = np.mean(imgs_b,axis=(0))
-        else:
-            path_b = f"{dir_b}/{fn}{suffix_b}.png"
-            assert os.path.exists(path_b), f'path {path_b} does not exist'
-            img_b = cv2.imread(path_b)
-        # print(img_b.shape)
-        
-        score += ssim(img_a,img_b, data_range=img_a.max() - img_b.min(), multichannel=True, channel_axis=2)
-        count +=1
-
-        if count % verbose_freq == 0 or count == len(fns):
-            print(f"{count}/{len(fns)}, running mean SSIM {score / count}")
-    return score/count
-
 def test_cli_inference_consistency(tmp_path, model_dir_final):
     dir_model = model_dir_final
     dir_input = 'Datasets/Sample_Dataset/test_cli'
@@ -98,11 +105,11 @@ def test_cli_inference_consistency(tmp_path, model_dir_final):
     print('num of input files (derived from output):',len(fns))
     print('input img name:',fns)
     
+    print(f'Calculating SSIM...')
     for i, suffix in enumerate(l_suffix):
-        print(f'Calculating {suffix}...')
         ssim_score = calculate_ssim(dirs_output[0], dirs_output[1], fns, '_'+suffix, '_'+suffix)
-        print(ssim_score)
-        assert ssim_score == 1
+        print(suffix, ssim_score)
+        assert (1 - ssim_score) < TOLERANCE
 
 def test_cli_inference_eager_consistency(tmp_path, model_dir_final):
     dir_model = model_dir_final
@@ -131,11 +138,11 @@ def test_cli_inference_eager_consistency(tmp_path, model_dir_final):
     print('num of input files (derived from output):',len(fns))
     print('input img name:',fns)
     
+    print(f'Calculating SSIM...')
     for i, suffix in enumerate(l_suffix):
-        print(f'Calculating {suffix}...')
         ssim_score = calculate_ssim(dirs_output[0], dirs_output[1], fns, '_'+suffix, '_'+suffix)
-        print(ssim_score)
-        assert ssim_score == 1
+        print(suffix, ssim_score)
+        assert (1 - ssim_score) < TOLERANCE
   
 
     
