Fix: calculate variance on tile image

deepliif/models/__init__.py
@@ -293,10 +293,11 @@ def is_empty_old(tile):
 
 
 def is_empty(tile):
+    thresh = 15
     if isinstance(tile, list): # for pair of tiles, only mark it as empty / no need for prediction if ALL tiles are empty
-        return all([True if np.max(image_variance_rgb(tile)) < 15 else False for t in tile])
+        return all([True if np.max(image_variance_rgb(t)) < thresh else False for t in tile])
     else:
-        return True if np.max(image_variance_rgb(tile)) < 15 else False
+        return True if np.max(image_variance_rgb(tile)) < thresh else False
 
 
 def run_wrapper(tile, run_fn, model_path, eager_mode=False, opt=None):